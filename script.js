7caddd58d520868dd44cf451cb07dcc845e8e3b8
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-05-12 17:12:03 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+const playListContainerTag = document.getElementsByClassName("playListContainer")[0];
+const audioTag = document.getElementsByClassName("audioPlayer")[0];
+const currentProgressTag = document.getElementById("currentProgress");
+const currentAndTotalTimeTag = document.getElementsByClassName("currentAndTotalTime")[0];
+const previousButtonTag = document.getElementsByClassName("previousButton")[0];
+const playButtonTag = document.getElementsByClassName("playButton")[0];
+const pauseButtonTag = document.getElementsByClassName("pauseButton")[0];
+const nextButtonTag = document.getElementsByClassName("nextButton")[0];
+const volumeButtonTag = document.getElementsByClassName("volumeButton")[0];
+
+const tracks = [
+    { trackId: "music/track1.mp3" , title: "December Nya - Lin Nit" },
+    { trackId: "music/track2.mp3" , title: "Tsaw Ra Ai Tsaw Hkrup Sai - Ann Naw" },
+    { trackId: "music/track3.mp3" , title: "Lann Mha Gyee Yey Bey - Wine Su Khine Thein" },
+    { trackId: "music/track4.mp3" , title: "Yee Zarr Sar - Sai Sai Kham Hlaing" },
+];
+
+for ( let i = 0; i < tracks.length ; i ++) {
+    const trackTag = document.createElement("div");
+    trackTag.addEventListener("click" , () => {
+        const trackId = tracks[i].trackId;
+        audioTag.src = trackId;
+        audioTag.play();
+        isPlaying = true;
+        updatePlayAndPauseButton();
+        currentPlayingIndex = i;
+    });
+    trackTag.classList.add("trackItem");
+    const title = (i + 1).toString() + ". " + tracks[i].title;
+    trackTag.textContent = title;
+    playListContainerTag.append(trackTag);
+};
+
+let duration = 0;
+let durationText = "00:00"; // current time မှာလဲ လှမ်းခေါ်ချင်လို့ Global Scope မှာ ကြေညာ
+audioTag.addEventListener("loadeddata" , () => {
+    duration = Math.floor(audioTag.duration); // 147.92938 သီချင်းရဲ့ ကြာချိန်ထည့်ချင်လို့
+    durationText = createMinuteAndSecondText(duration);
+});
+
+audioTag.addEventListener("timeupdate" , () => {
+    const currentTime = Math.floor(audioTag.currentTime); //သီချင်းဖွင့်နေတဲ့ ကြာချိန်ထည့်ချင်လို့
+    const currentTimeText =  createMinuteAndSecondText(currentTime);
+    const currentTimeTextAndDurationText = currentTimeText + " / " + durationText;
+    currentAndTotalTimeTag.textContent = currentTimeTextAndDurationText;
+    updateCurrentPrgress(currentTime);
+});
+
+const updateCurrentPrgress = (currentTime) => {
+    const currentProgressWidth = (500/duration) * currentTime;
+    currentProgressTag.style.width = currentProgressWidth.toString() + "px";
+};
+
+const createMinuteAndSecondText = (totalSecond) => {
+    const minutes = Math.floor(totalSecond / 60);
+    const seconds = totalSecond % 60;
+
+    const minuteText = minutes < 10 ? "0" + minutes.toString() : minutes;
+    const secondText = seconds < 10 ? "0" + seconds.toString() : seconds;
+    return minuteText + ":" + secondText;
+};
+
+let currentPlayingIndex = 0;
+let isPlaying = false;
+playButtonTag.addEventListener("click" , () => {
+    const currentTime = Math.floor(audioTag.currentTime);
+    isPlaying = true;
+    if (currentTime === 0) {
+        const songIdToPlay = tracks[currentPlayingIndex].trackId;
+        audioTag.src = songIdToPlay;
+        audioTag.play();
+        updatePlayAndPauseButton();
+    } else {
+        audioTag.play();
+        updatePlayAndPauseButton();
+    }    
+});
+
+pauseButtonTag.addEventListener("click" , () => {
+    isPlaying = false;
+    audioTag.pause();
+    updatePlayAndPauseButton();
+});
+
+previousButtonTag.addEventListener("click" , () => {
+    if (currentPlayingIndex === 0) {
+        return;
+    }
+    currentPlayingIndex -= 1;
+    const songIdToPlay = tracks[currentPlayingIndex].trackId;
+    audioTag.src = songIdToPlay;
+    audioTag.play();
+    isPlaying = true;
+    updatePlayAndPauseButton();
+});
+
+nextButtonTag.addEventListener("click" , () => {
+    if (currentPlayingIndex === tracks.length - 1) {
+        return;
+    }
+    currentPlayingIndex += 1;
+    const songIdToPlay = tracks[currentPlayingIndex].trackId;
+    audioTag.src = songIdToPlay;
+    audioTag.play();
+    isPlaying = true;
+    updatePlayAndPauseButton();
+});
+
+const updatePlayAndPauseButton = () => {
+    if (isPlaying) {
+        playButtonTag.style.display = "none";
+        pauseButtonTag.style.display = "inline";
+    } else {
+        playButtonTag.style.display = "inline";
+        pauseButtonTag.style.display = "none";
+    }
+};
